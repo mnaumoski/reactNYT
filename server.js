4c5a2742374f5d2538c59cd73a0bc964fd901afe
mnaumoski <naumoski.marija@gmail.com>
2016-12-15 14:58:16 -0500

    started building server

---
+++ server.js
@@ -0,0 +1,59 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Require Article schema
+var Article = require("./Models/Article");
+// Create a new express app
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./public"));
+
+// -------------------------------------------------
+
+// MongoDB configuration (Change this URL to your own DB)
+mongoose.connect("mongodb://localhost/nytreact");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// -------------------------------------------------
+
+// Main "/" Route. This will redirect the user to our rendered React application
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// This is the route we will send GET requests to retrieve our saved articles.
+// We will call this route the moment our page gets rendered
+app.get("/api/saved", function(req, res) {
+
+  // This GET request will search for all articles saved
+  Article.find({}).exec(function(err, doc) {
+
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+
+
